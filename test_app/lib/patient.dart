0d8c6b729498ece4c1cf01452e24552ea1108711
Sumukhi Mathapati <mathapathisumukhi@gmail.com>
2024-06-04 13:55:12 -0400

    demographics

--- test_app/lib/patient.dart
+++ test_app/lib/patient.dart
@@ -2,11 +2,23 @@ class Patient {
   final String id;
   final String firstName;
   final String lastName;
+  final String address1;
+  final String city;
+  final String state;
+  final String zipCode;
+  final String email;
+  final String birthDate;
 
   Patient({
     required this.id,
     required this.firstName,
     required this.lastName,
+    this.address1 = '',
+    this.city = '',
+    this.state = '',
+    this.zipCode = '',
+    this.email = '',
+    this.birthDate = '',
   });
 
   factory Patient.fromJson(Map<String, dynamic> json) {
@@ -14,6 +26,12 @@ class Patient {
       id: json['pat_id'],
       firstName: json['first_name'],
       lastName: json['last_name'],
+      address1: json['address1'] ?? '',
+      city: json['city'] ?? '',
+      state: json['state'] ?? '',
+      zipCode: json['zip_code'] ?? '',
+      email: json['email'] ?? '',
+      birthDate: json['birth_date'] ?? '',
     );
   }
 }
--- test_app/lib/patient_list_screen.dart
+++ test_app/lib/patient_list_screen.dart
@@ -13,25 +13,27 @@ class PatientListScreen extends StatelessWidget {
         title: const Text('Patient List'),
       ),
       body: GridView.builder(
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 3, // Number of columns
-          crossAxisSpacing: 4, // Horizontal space between items
-          mainAxisSpacing: 4, // Vertical space between items
-          childAspectRatio: 3 / 2, // Aspect ratio of each item
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3, // Adjusted for better readability
+          crossAxisSpacing: 4,
+          mainAxisSpacing: 4,
+          childAspectRatio: 3/2,
         ),
         itemCount: patients.length,
         itemBuilder: (_, index) {
           return Card(
             child: Container(
-              padding: EdgeInsets.all(8),
+              padding: const EdgeInsets.all(8),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
                   Text('${patients[index].firstName} ${patients[index].lastName}',
-                      style: TextStyle(fontWeight: FontWeight.bold)),
-                  SizedBox(height: 8),
-                  Text('ID: ${patients[index].id}'),
-                  // You can add more patient details here
+                      style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
+                  Text('ID: ${patients[index].id}', style: TextStyle(color: Colors.grey[600])),
+                  Text('Address: ${patients[index].address1}, ${patients[index].city}, ${patients[index].state} ${patients[index].zipCode}'),
+                  Text('Email: ${patients[index].email}'),
+                  Text('Birth Date: ${patients[index].birthDate}'),
                 ],
               ),
             ),
