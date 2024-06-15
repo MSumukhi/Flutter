a9ade66ff11943f56aeb2cf9eca10a0ca1011a03
Sumukhi Mathapati <mathapathisumukhi@gmail.com>
2024-06-14 20:37:36 -0400

    vitals from observation

--- test_app/lib/main.dart
+++ test_app/lib/main.dart
@@ -27,45 +27,11 @@ Future<void> authenticateUser(String username, String password) async {
   }
 }
 
-// Function to retrieve patient data
-Future<void> getPatientData() async {
-  if (bearerToken != null) {
-    try {
-      final String encodedOperation = base64Encode(utf8.encode('GET/db/patients'));
-
-      final response = await http.get(
-        Uri.parse('$apiUrl/$encodedOperation'),
-        headers: {'Authorization': 'Bearer $bearerToken', 'Accept': 'application/json'},
-      );
-
-      print('Get patient data response status code: ${response.statusCode}');
-
-      if (response.statusCode == 200) {
-        final List<dynamic> patients = jsonDecode(response.body)['db'];
-        for (var patient in patients) {
-          print('Patient ID: ${patient['pat_id']}');
-          print('First Name: ${patient['first_name']}');
-          print('Last Name: ${patient['last_name']}');
-          // Print other patient details as needed
-          print('--------------------------------');
-        }
-      } else {
-        print('Failed to retrieve patient data: ${response.statusCode}');
-      }
-    } catch (e) {
-      print('Error retrieving patient data: $e');
-    }
-  } else {
-    print('Bearer token not available. Cannot make request for patient data.');
-  }
-}
-
-// Function to retrieve observation data
+// Function to retrieve observation data with filter for vitals
 Future<void> getObservationData() async {
   if (bearerToken != null) {
     try {
       final String encodedOperation = base64Encode(utf8.encode('GET/db/observations'));
-
       final response = await http.get(
         Uri.parse('$apiUrl/$encodedOperation'),
         headers: {'Authorization': 'Bearer $bearerToken', 'Accept': 'application/json'},
@@ -76,35 +42,16 @@ Future<void> getObservationData() async {
       if (response.statusCode == 200) {
         final List<dynamic> observations = jsonDecode(response.body)['db'];
         for (var observation in observations) {
-          print('Observation ID: ${observation['obs_id']}');
-          print('Patient ID: ${observation['pat_id']}');
-          print('Observation Name: ${observation['obs_name']}');
-          print('Observation Result: ${observation['obs_result']}');
-          print('Observer ID: ${observation['observer_id']}');
-          print('Observed DateTime: ${observation['observed_datetime']}');
-          print('Observation Code: ${observation['obs_code']}');
-          print('Template ID: ${observation['template_id']}');
-          print('Revision Number: ${observation['revision_number']}');
-          print('User ID: ${observation['user_id']}');
-          print('Observation Order: ${observation['obs_order']}');
-          print('Observation Range: ${observation['obs_range']}');
-          print('Observation Units: ${observation['obs_units']}');
-          print('Observation Flag: ${observation['obs_flag']}');
-          print('Observation Status: ${observation['obs_status']}');
-          print('Verified DateTime: ${observation['verified_datetime']}');
-          print('Restricted: ${observation['restricted']}');
-          print('Create DateTime: ${observation['create_datetime']}');
-          print('Modified DateTime: ${observation['modified_datetime']}');
-          print('Interface: ${observation['interface']}');
-          print('Observation Ext ID: ${observation['obs_ext_id']}');
-          print('Test Comments: ${observation['test_comments']}');
-          print('Free Text: ${observation['free_text']}');
-          print('Micro Result: ${observation['micro_result']}');
-          print('Interpretive Text: ${observation['interpretive_text']}');
-          print('Inpatient: ${observation['inpatient']}');
-          print('Observed Start TS: ${observation['observed_start_ts']}');
-          print('Observed End TS: ${observation['observed_end_ts']}');
-          print('--------------------------------');
+          // Filtering for vitals-related observation names
+          if (['Height', 'Weight', 'BMI', 'Blood Pressure', 'Pulse', 'Temp', 'Resp', 'O2 Sat'].contains(observation['obs_name'])) {
+            print('Observation ID: ${observation['obs_id']}');
+            print('Patient ID: ${observation['pat_id']}');
+            print('Observation Name: ${observation['obs_name']}');
+            print('Observation Result: ${observation['obs_result']}');
+            print('Observed DateTime: ${observation['observed_datetime']}');
+            print('Observation Units: ${observation['obs_units']}');
+            print('--------------------------------');
+          }
         }
       } else {
         print('Failed to retrieve observation data: ${response.statusCode}');
@@ -123,6 +70,5 @@ void main() async {
   final String password = 'Sumukhi@1231';
 
   await authenticateUser(username, password);
-  await getPatientData();
   await getObservationData();
 }
