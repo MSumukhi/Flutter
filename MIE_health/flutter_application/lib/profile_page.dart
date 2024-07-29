507c0507bb85e25b1f971204d250242eee580ecd
MSumukhi <maths01@pfw.edu>
2024-07-29 10:24:38 -0400

    UI/UX and User Prfole Integration

---
+++ MIE_health/flutter_application/lib/profile_page.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ProfilePage extends StatefulWidget {
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  TextEditingController _nameController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _loadUserProfile();
+  }
+
+  Future<void> _loadUserProfile() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _nameController.text = prefs.getString('userName') ?? '';
+    });
+  }
+
+  Future<void> _saveUserProfile() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString('userName', _nameController.text);
+    Navigator.of(context).pop(); // Go back to the previous screen
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Profile'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _nameController,
+              decoration: InputDecoration(
+                labelText: 'Name',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _saveUserProfile,
+              child: Text('Save'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
